56b0a2bff4263d6b928a0c5fecf251880c22c348
Usuario <Usuario@10.22.148.82>
2019-09-25 14:19:49 +0100

    Kata1 using LocalDate

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,13 @@
+package kata1;
+
+import java.time.LocalDate;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        Person Jeremy = new Person("Jeremy Trujillo", LocalDate.of(1999,10,23));
+        System.out.println(Jeremy);
+        
+    }
+    
+}
---
+++ src/kata1/Person.java
@@ -0,0 +1,31 @@
+package kata1;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthday;
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+        public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", birthday=" + birthday + ", age=" + getAge() + '}';
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+    
+    public int getAge(){
+        return Period.between(birthday,LocalDate.now()).getYears(); 
+    }
+}
